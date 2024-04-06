93a96b1d757009331dfef3de595e8c9afe072b5b
shanka236 <106525515+shanka236@users.noreply.github.com>
2024-04-06 11:57:05 +0530

    Add files via upload

---
+++ stopwatch/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import StopWatch from "./Component/stopWatch";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <StopWatch />
+      </div>
+    </div>
+  );
+}
+
+export default App;
